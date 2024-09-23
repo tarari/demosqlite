e4514ef533f2cbb3de14955982512c4e5bf969a7
tarari <t.jimenez@escolesnuria.cat>
2024-09-23 19:07:13 +0200

    index.php Review

--- config.php
+++ config.php
@@ -2,3 +2,10 @@
 
     //constantes
     define('VIEWS',__DIR__.'/src/views');
+    define('CONTR',__DIR__.'/src/controllers');
+    define('MODS',__DIR__.'/src/models');
+    $routes=[
+        'home',
+        'add',
+        'admin'
+    ];
